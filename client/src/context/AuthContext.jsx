0e7735480dd2e655a7950bc291225f246c7b9590
HinduPatrini <hindupatrini16@gmail.com>
2026-04-03 13:04:38 +0530

    changed

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const backendUrl =
-  import.meta.env.VITE_BACKEND_URL || "http://your-backend.onrender.com";
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 axios.defaults.baseURL = backendUrl;
 
